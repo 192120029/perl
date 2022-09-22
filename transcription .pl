1f719790b03ee77dffb63632218ce5e3a09b5921
A.Manju sri <112066824+192120029@users.noreply.github.com>
2022-09-22 21:18:20 +0530

    Delete transcription .pl

--- transcription .pl
+++
@@ -1,8 +0,0 @@
-$DNA = 'ACGGGAGGACTGCATGCATGCGGGAAAATTACTACGGCATTAGC';
-print "Here is the starting DNA:\n\n";
-print "$DNA\n\n";
-$RNA = $DNA;
-$RNA =~ s/T/U/g;
-print "Here is the result of transcribing the DNA to RNA:\n\n";
-print "$RNA\n";
-exit;
